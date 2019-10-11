2e5f9e69b41d52a9e1789cd0f21b06d85310fac2
adgray09 <alexandergray37@yahoo.com>
2019-10-10 21:23:29 -0700

    more tests

--- test.py
+++ test.py
@@ -4,6 +4,14 @@ from app import app
 class ContractorTests(TestCase):
     """Flask tests."""
 
+
+sample_chip = {
+    'title': 'Salt and Vinegar',
+    'description': 'Cats acting weird',
+    'url': [
+    'https://i5.walmartimages.com/asr/2d982a8d-39aa-4f78-a2a8-9ac9dae0f004_1.d97af068ee3d60be94301b502f074e7a.jpeg'
+]
+    
     def setUp(self):
         """Stuff to do before every test."""
 
@@ -24,6 +32,13 @@ class ContractorTests(TestCase):
         result = self.client.get('/')
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'Chip', result.data)
+        
+    @mock.patch('pymongo.collection.Collection.update_one')
+    def test_chips_edit(self, mock_update):
+        result = self.client.post(f'/playlists/{sample_chip_id}', data=sample_form_data)
+
+        self.assertEqual(result.status, '302 FOUND')
+        mock_update.assert_called_with({'_id': sample_chip_id}, {'$set': sample_playlist})
     
 if __name__ == '__main__':
     unittest_main()
